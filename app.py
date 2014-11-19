c977f05dbca46ab63cc1bc058504476bdd762c10
john <gatarijoris@gmail.com>
2014-11-19 09:33:06 +0200

    starting the flask application

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/kigali')
+def index():
+    return "  I am in time the capital of Rwanda"
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
+
+@app.route('/')
+def index1():
+    return '<h1>Hello World!</h1>'
